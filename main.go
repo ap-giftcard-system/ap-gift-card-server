29396fbda8c903ce130ff488e0bfc3f0f9bad390
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-22 17:12:54 -0500

    update: updated main.go code spacing

--- main.go
+++ main.go
@@ -13,37 +13,37 @@ import (
 
 // @notice: global variables
 var (
-  server			*gin.Engine
-  postgresClient		*gorm.DB
+	server			*gin.Engine
+	postgresClient		*gorm.DB
 )
 
 // @dev Runs before main()
 func init() {
-  // load env variables
-	if (os.Getenv("GIN_MODE") != "release") {utils.LoadEnvVars()}
-  
-  // set up gin engine
-  server = gin.Default()
+	// load env variables
+		if (os.Getenv("GIN_MODE") != "release") {utils.LoadEnvVars()}
+	
+	// set up gin engine
+	server = gin.Default()
 
-  // Gin trust all proxies by default and it's not safe. Set trusted proxy to home router to to mitigate 
-  server.SetTrustedProxies([]string{os.Getenv("HOME_ROUTER")})
+	// Gin trust all proxies by default and it's not safe. Set trusted proxy to home router to to mitigate 
+	server.SetTrustedProxies([]string{os.Getenv("HOME_ROUTER")})
 
-  // init postgres client
-  postgresClient = db.EstablishPostgresClient()
+	// init postgres client
+	postgresClient = db.EstablishPostgresClient()
 }
 
 // @dev Root function
 func main() {
-  // Catch all unallowed HTTP methods sent to the server
+  	// Catch all unallowed HTTP methods sent to the server
 	server.HandleMethodNotAllowed = true
 
-  // init basePath
+  	// init basePath
 	giftsBasePath := server.Group("/v1/ap/gifts/")
 
-  // init Handler
-  routers.ApRouter(giftsBasePath)
+	// init Handler
+	routers.ApRouter(giftsBasePath)
 
-  // run gin server engine
+	// run gin server engine
 	if (os.Getenv("GIN_MODE") != "release") {
 		server.Run(os.Getenv("LOCAL_DEV_PORT"))
 	} else {
