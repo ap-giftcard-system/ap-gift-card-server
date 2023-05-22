48ab13ca14c32b27cacae3ba903f3005315ee18b
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-22 14:52:44 -0500

    feat: added LoadEnvVars() utils

--- utils/ap.utils.go
+++ utils/ap.utils.go
@@ -1,2 +1,15 @@
 package utils
 
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+// @dev Loads environment variables
+func LoadEnvVars() {
+	err := godotenv.Load();
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
