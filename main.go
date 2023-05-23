3466b64ac1807043670ae14ea3894f392fa828eb
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-22 19:15:12 -0500

    update: added mongo connection to main.go

--- main.go
+++ main.go
@@ -2,16 +2,21 @@ package main
 
 // @import
 import (
+	"ap-gift-card-server/db"
 	"ap-gift-card-server/routers"
 	"ap-gift-card-server/utils"
+	"context"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // @notice: global variables
 var (
 	server			*gin.Engine
+	ctx			context.Context
+	mongoClient		*mongo.Client
 )
 
 // @dev Runs before main()
@@ -25,12 +30,21 @@ func init() {
 	// Gin trust all proxies by default and it's not safe. Set trusted proxy to home router to to mitigate 
 	server.SetTrustedProxies([]string{os.Getenv("HOME_ROUTER")})
 
+	// init context
+	ctx = context.TODO()
+
+	// init mongo client
+	mongoClient = db.EstablishMongoClient(ctx)
+
 }
 
 // @dev Root function
 func main() {
   	// Catch all unallowed HTTP methods sent to the server
 	server.HandleMethodNotAllowed = true
+
+	// defer a call to `Disconnect()` after instantiating client
+	defer func() {if err := mongoClient.Disconnect(ctx); err != nil {panic(err)}}()
 
   	// init basePath
 	giftsBasePath := server.Group("/v1/ap/gifts/")
