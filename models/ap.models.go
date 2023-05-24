f6a28d27363c6915d468204eb5c9ef000ab0f47b
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-24 09:31:13 -0500

    update: removed ID and DeletedAt fields in ap.models

--- models/ap.models.go
+++ models/ap.models.go
@@ -4,7 +4,6 @@ import "time"
 
 // @notice struct for AP Gift Holder
 type ApGiftHolder struct {
-	ID string			`bson:"_id" validate:"omitempty"`
 	BarCode string 			`json:"barCode" bson:"bar_code" validate:"required"`
 	HolderName string 		`json:"holderName" bson:"holder_name" validate:"required"`
 	HolderPhone string		`json:"holderPhone" bson:"holder_phone" validate:"omitempty,e164"` // e164Pattern = `^\+\d{1,15}$`
@@ -12,5 +11,4 @@ type ApGiftHolder struct {
 	GiftAmount float64		`json:"giftAmount" bson:"gift_amount" validate:"required,number"`
 	CreatedAt time.Time 		`json:"createdAt" bson:"created_at,omitempty" validate:"omitempty"`
 	UpdatedAt time.Time 		`json:"updateAt" bson:"updated_at,omitempty" validate:"omitempty"`
-	DeletedAt time.Time 		`json:"deleteAt" bson:"deleted_at,omitempty" validate:"omitempty"`
 }
