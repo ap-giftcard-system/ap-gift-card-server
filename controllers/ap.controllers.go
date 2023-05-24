9ce8aaf0d030a64abffe8bd3140f7516679a641c
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-24 08:24:49 -0500

    update: finished RegisterNewApGiftHoder controller method

--- controllers/ap.controllers.go
+++ controllers/ap.controllers.go
@@ -2,9 +2,15 @@ package controllers
 
 import (
 	"ap-gift-card-server/dao"
+	"ap-gift-card-server/models"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
+
+// @notice global var
+var validate = validator.New()
 
 // @notice Root struct for other methods in controller
 type ApGiftController struct {
@@ -24,7 +30,52 @@ func ApGiftControllerContructor(apGiftDao *dao.ApGiftDao) *ApGiftController {
 // 
 // @param gc *gin.Context
 func (agc *ApGiftController) RegisterNewApGiftHoder(gc *gin.Context) {
-	gc.JSON(200, "true")
+	// prepare `param` placeholder
+	param := &models.ApGiftHolder{}
+
+	// bind json post data to `param`
+	if err := gc.ShouldBindJSON(param); err != nil {
+		gc.AbortWithStatusJSON(400, gin.H{"error": gin.H{
+			"key": "!BAD_REQUEST",
+			"msg": err.Error(),
+		}}); return;
+	}
+
+	// validate struct
+	if err := validate.Struct(param); err != nil {
+		gc.AbortWithStatusJSON(400, gin.H{"error": gin.H{
+			"key": "!BAD_REQUEST",
+			"msg": err.Error(),
+		}}); return;
+	}
+
+	// @logic post data must have either HolderPhone or HolderEmail
+	if strings.EqualFold(param.HolderEmail, "") && strings.EqualFold(param.HolderPhone, "") {
+		gc.AbortWithStatusJSON(400, gin.H{"error": gin.H{
+			"key": "!MISSING_CONTACT", 
+			"msg": "Gift Holder must provide at least either email or phone information for contacting purposes",
+		}}); return;
+	}
+
+	// invoke dao.RegisterNewApGiftHoder
+	if err := agc.ApGiftDao.RegisterNewApGiftHoder(param); err != nil {
+		// @logic abort request with a conflict status if Gift Holder already exists
+		if strings.EqualFold(err.Error(), "mongo: existed document found in result") {
+			gc.AbortWithStatusJSON(409, gin.H{"error": gin.H{
+				"key": "!INTERNAL_SERVER",
+				"msg": err.Error(),
+			}}); return;
+		} else {
+			// @logic abort request with a 500 if there's a unknown error from internal database
+			gc.AbortWithStatusJSON(500, gin.H{"error": gin.H{
+				"key": "!INTERNAL_SERVER",
+				"msg": err.Error(),
+			}}); return;
+		}
+	}
+	
+	// return 200 ok to client
+	gc.JSON(200, gin.H{"error": nil})
 }
 
 // @route `PUT/update`
