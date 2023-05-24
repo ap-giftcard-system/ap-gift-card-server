aab9e8400b3a6d1d79ea6dd92d11bf5a2df86237
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-24 12:09:10 -0500

    update: finished v1 UpdateApGiftHolder DAO API

--- dao/ap.dao-impl.go
+++ dao/ap.dao-impl.go
@@ -67,7 +67,37 @@ func (gdi *ApGiftDaoImpl) RegisterNewApGiftHoder(giftHolder *models.ApGiftHolder
 // 
 // @return error
 func (gdi *ApGiftDaoImpl) UpdateApGiftHolder(giftHolder *models.ApGiftHolder) (error) {
-	return nil
+	// prepare filter
+	filter := bson.D{{Key: "_id", Value: giftHolder.GiftHolderID}}
+
+	// find the document matching `filter`
+	dbRes := gdi.mongoCollection.FindOne(gdi.ctx, filter)
+
+	// @logic if dbRes.Err() == nil => document found => update document
+	// @logic if dbRes.Err() == mongo.ErrNoDocuments => document not found => 404 !OK
+	// @logic else => unknown error occurs => 500
+	if dbRes.Err() == nil {
+		// prepare update query
+		update := bson.D{
+			{Key: "$set", Value: bson.D{{Key: "bar_code", Value: giftHolder.BarCode}}},
+			{Key: "$set", Value: bson.D{{Key: "holder_name", Value: giftHolder.HolderName}}},
+			{Key: "$set", Value: bson.D{{Key: "holder_phone", Value: giftHolder.HolderPhone}}},
+			{Key: "$set", Value: bson.D{{Key: "holder_email", Value: giftHolder.HolderEmail}}},
+			{Key: "$set", Value: bson.D{{Key: "gift_amount", Value: giftHolder.GiftAmount}}},
+			{Key: "$set", Value: bson.D{{Key: "updated_at", Value: time.Now()}}},
+		}
+
+		// update document
+		if _, err := gdi.mongoCollection.UpdateOne(gdi.ctx, filter, update); err != nil {
+			return errors.New("cannot update document")
+		} else {
+			return nil
+		}
+	} else if dbRes.Err() == mongo.ErrNoDocuments {
+		return errors.New("ErrNoDocuments")
+	} else {
+		return dbRes.Err()
+	}
 }
 
 // @dev Get a list of all ApGiftHolders in internal database
