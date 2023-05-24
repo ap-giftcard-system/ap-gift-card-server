f633c6bb39c304acdc9aadf41e6df47a70894138
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-24 08:23:26 -0500

    update: udpated ap.models

--- models/ap.models.go
+++ models/ap.models.go
@@ -4,12 +4,12 @@ import "time"
 
 // @notice struct for AP Gift Holder
 type ApGiftHolder struct {
-	ID string			`bson:"_id"`
+	ID string			`bson:"_id" validate:"omitempty"`
 	BarCode string 			`json:"barCode" bson:"bar_code" validate:"required"`
 	HolderName string 		`json:"holderName" bson:"holder_name" validate:"required"`
-	HolderPhone string		`json:"holderPhone" bson:"holder_phone" validate:"omitempty,e164"`
+	HolderPhone string		`json:"holderPhone" bson:"holder_phone" validate:"omitempty,e164"` // e164Pattern = `^\+\d{1,15}$`
 	HolderEmail string		`json:"holderEmail" bson:"holder_email" validate:"omitempty,email"`
-	GiftAmount uint64		`json:"giftAmount" bson:"gift_amount" validate:"required,number"`
+	GiftAmount float64		`json:"giftAmount" bson:"gift_amount" validate:"required,number"`
 	CreatedAt time.Time 		`json:"createdAt" bson:"created_at,omitempty" validate:"omitempty"`
 	UpdatedAt time.Time 		`json:"updateAt" bson:"updated_at,omitempty" validate:"omitempty"`
 	DeletedAt time.Time 		`json:"deleteAt" bson:"deleted_at,omitempty" validate:"omitempty"`
