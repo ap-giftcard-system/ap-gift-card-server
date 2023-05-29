def7771f907721edb82fa8f34285954371b66ba4
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-28 23:11:29 -0500

    udpate: updated ApModels

--- models/ap.models.go
+++ models/ap.models.go
@@ -9,7 +9,7 @@ type ApGiftHolder struct {
 	HolderName string 		`json:"holderName" bson:"holder_name" validate:"required"`
 	HolderPhone string		`json:"holderPhone" bson:"holder_phone" validate:"omitempty,e164"` // e164Pattern = `^\+\d{1,15}$`
 	HolderEmail string		`json:"holderEmail" bson:"holder_email" validate:"omitempty,email"`
-	GiftAmount float64		`json:"giftAmount" bson:"gift_amount" validate:"required,number"`
+	GiftAmount float64		`json:"giftAmount" bson:"gift_amount" validate:"omitempty,number"`
 	CreatedAt time.Time 		`json:"createdAt" bson:"created_at,omitempty" validate:"omitempty"`
 	UpdatedAt time.Time 		`json:"updateAt" bson:"updated_at,omitempty" validate:"omitempty"`
 }
